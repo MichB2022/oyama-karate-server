dd69c307a22af2dc5d0789b04ada37bef82a715a
Jakub Pisula <jakubpisula@macbook-pro-jakub-2.home>
2022-03-19 22:43:33 +0100

    aaa

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 var whitelist = [
   'http://localhost:3000',
   'http://localhost:3001',
-  'https://oyama-karate-next-ka8lz685b-jpisula.vercel.app'
+  'https://oyama-karate-next-p75wa3o3z-jpisula.vercel.app/'
 ]; //white list consumers
 var corsOptions = {
   origin: function (origin, callback) {
